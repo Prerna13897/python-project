7605badc1284c5e3ac5d0c2f4a887a78bdc2579c
prerna13897 <prerna13897@gmail.com>
2017-03-24 15:59:21 +0530

    project 1 completed

---
+++ main.py
@@ -0,0 +1,7 @@
+a=raw_input("enter 1 for students_to_teacher,2 for battleship and 3 for exam_stats")
+if a=='1':
+    import students_to_teacher
+elif a=='2':
+    import battleship
+else:
+    import exam_stats
